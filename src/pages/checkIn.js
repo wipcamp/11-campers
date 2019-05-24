7c92ee743d8a5ded79c89770f1f5ffb9ea0aba66
Mild-TN <mild20091931@gmail.com>
2019-05-24 23:48:51 +0700

    [UPDATE] send props pass to Card Component

--- src/pages/checkIn.js
+++ src/pages/checkIn.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Card from '../component/Core/Card';
 import { Container, Row, Col } from 'reactstrap';
 import socketIOClient from 'socket.io-client'
+import RuleText from '../component/Core/RuleWiFiText'
+
 const socket = socketIOClient('http://localhost:3002')
 class checkIn extends Component {
 
@@ -9,10 +11,18 @@ class checkIn extends Component {
     socket.on('personIdClient', (res) => {
       console.log(res)
     })
+
     return (
       <Container>
         <Row>
-          <Col sm="12" md={{ size: 8, offset: 2 }}><Card title="Check-in Wip Camp #11" /></Col>
+          <Col sm="12" md={{ size: 8, offset: 2 }} className="mt-4 p-2">
+            <Card 
+            title="Wip Camp #11" 
+            text={<RuleText />} 
+            subtitle="พรบ.คอมพิวเตอร์" 
+            textBtn="ดำเนินการต่อ"
+            position="row justify-content-end"/>
+          </Col>
         </Row>
       </Container>
     );
