10db7c12adf4cd58dd26fedc8ed29a42237d4d8e
Mild-TN <mild20091931@gmail.com>
2019-05-24 21:37:23 +0700

    [ADD] config/color.js

---
+++ src/config/colors.js
@@ -0,0 +1,14 @@
+export default {
+  primary: '#304151',
+  primaryDarker: '#304155',
+  primaryHover: '#0098DA',
+  primaryShadow: '',
+  error: '#F5222D',
+  errorShadow: '',
+  grayShadow: '',
+  normalText: '#000000',
+  placeHolderText: '#BFBFBF',
+  borderColor: '#D9D9D9',
+  hover: '#F0F0F0',
+  bgcolor: 'linear-gradient(#fff,#F8E9D6)'
+}
