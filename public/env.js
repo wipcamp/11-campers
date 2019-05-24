5ce4c8bfba327f25a37f5e3d5e4049d9ccb7bf87
pepea <pepea.23@live.com>
2019-05-24 20:39:38 +0700

    [UPDATE] env + socket + axios

---
+++ public/env.js
@@ -0,0 +1,9 @@
+window.env = {
+    PORT_BE:5000,
+    PORT:3002,
+    PATH_AUTH:'https://auth.service.wip.camp/api',
+    PATH_REGISTANCE:'https://registrant.service.wip.camp/api',
+}
+
+
+
--- src/pages/checkIn.js
+++ src/pages/checkIn.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-
+import socketIOClient from 'socket.io-client'
+const socket = socketIOClient('http://localhost:3002')
 class checkIn extends Component {
+  
   render() {
+    socket.on('personIdClient' ,(res) => {
+      console.log(res)
+    })
     return (
       <div>
-        Hi
+        
       </div>
     );
   }
